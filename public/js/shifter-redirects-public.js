7557bfe104e2ed18a289b240be962e588b228327
Daniel Olson <emaildano@gmail.com>
2019-06-27 14:57:03 -0400

    testig redirect fules

--- public/js/shifter-redirects-public.js
+++ public/js/shifter-redirects-public.js
@@ -4,10 +4,17 @@
 	const data = shifterRedirects.data;
 	const source = data.shifter_redirects_source;
 	const destination = data.shifter_redirects_destination;
+	const host = window.location.host;
 
 	
 	if (destination && source) {
-		if (window.location.host !== destination) {
+		// if (host !== source) {
+		// 	return;
+		// }
+		// if (host === destination) {
+		// 	return;
+		// }
+		if (host === source) {
 			window.location = destination + window.location.pathname + window.location.search;
 		}
 	}
